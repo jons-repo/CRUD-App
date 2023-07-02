58122df41763d82391a829a4ebc5ed010c6f1b50
Kyaw Nanda <kyawnanda.lucky@gmail.com>
2023-07-02 02:24:20 -0400

    rearrange files + editied allStudent to show the results of list of all students

--- src/components/studentListing.jsx
+++ src/components/studentListing.jsx
@@ -1,24 +1,21 @@
-import React from 'react'
+import React from 'react';
+import StudentCard from './studentCard';
 
-const StudentCard = ({StudentCard}) => {
-    console.log(StudentCard)
-    return(
-      <div>
-
-        <div className="card" style={{ width: "18rem" }}>
-          <img src="https://img.freepik.com/free-vector/college-building-educational-institution-banner_1441-3616.jpg?size=626&ext=jpg" className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">{StudentCard.name}</h5>
-          </div>
-          <div className="card-body">
-            <a href="#" className="card-link">Edit</a>
-            <a href="#" className="card-link">Delete</a>
-          </div>
-        </div>
-
-      </div>
-    );
-
+const ListStudents = (props) => {
+  console.log("at the propslist",props.list);
+  return props.list ? (
+        props.list.map((studentCard) => {
+            console.log(studentCard)
+            return(
+                <div>
+                    <StudentCard studentCard = {studentCard} />
+                </div>
+            );
+        })
+    )
+    : (
+    <h1>Loading ...</h1>
+  );
 }
 
-export default StudentCard;
+export default ListStudents;

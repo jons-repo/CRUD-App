2ef889afd6bf5be78c11eb04b56d72b1bbc1e670
MohammedF425 <mohammedfadel425@gmail.com>
2023-07-03 11:14:03 -0400

    work on the css on homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,14 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h1>Home Page</h1>
+      <div class="banner">
+        <img src="https://www.cuny.edu/wp-content/uploads/sites/4/page-assets/about/administration/offices/communications-marketing/university-identity/cuny-name-logo.jpg" alt="CUNY banner"/>
+      </div>
+
+      <h1>Welcome to City Universities of New York Unofficial Site</h1>
+      <p>This site was made to keep track of our all campuses and students. In the campuses page you can find all of our campuses. Add campus allows one
+        to add a campus into the database. Students page displays all of our students. Add students allows one to add a student into the database" The 
+        site was made by Kyaw Nanda, Jonathan S, and Mohammed Fadel </p>
     </div>
   );
 };

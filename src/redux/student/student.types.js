77c8b4e244e132807609cdc5a518a08bd5c37ee7
jons-repo <s.jonathan4252@gmail.com>
2023-07-04 17:56:19 -0400

    After form edit, navigate back to students page to show the change.

--- src/redux/student/student.types.js
+++ src/redux/student/student.types.js
@@ -1,6 +1,8 @@
-const StudentActionType = {
+const StudentActionTypes = {
     FETCH_ALL_STUDENTS: "FETCH_ALL_STUDENTS",
-    ADD_STUDENT: "ADD_STUDENT"
-}
+    ADD_STUDENT: "ADD_STUDENT",
+    UPDATE_STUDENT: "UPDATE_STUDENT",
+    DELETE_STUDENT: "DELETE_STUDENT",
+};
 
-export default StudentActionType;
+export default StudentActionTypes;

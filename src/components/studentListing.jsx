c39efc97ea858860e16338c0eb923214cad447b6
MohammedF425 <mohammedfadel425@gmail.com>
2023-07-05 13:27:34 -0400

    added informative message for students listing when there are no students

--- src/components/studentListing.jsx
+++ src/components/studentListing.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 import StudentCard from './studentCard';
 
 const ListStudents = (props) => {
-  console.log("at the propslist",props.list);
-  return props.list ? (
-        props.list.map((studentCard) => {
-            console.log(studentCard)
-            return(
-                <div class="card-padding">
-                    <StudentCard studentCard = {studentCard} />
-                </div>
-            );
-        })
-    )
-    : (
-    <h1>Loading ...</h1>
-  );
-}
+    console.log("at the propslist", props.list);
+    return props.list && props.list.length > 0 ? (
+      props.list.map((studentCard) => {
+        console.log(studentCard);
+        return (
+          <div class="card-padding">
+            <StudentCard studentCard={studentCard} />
+          </div>
+        );
+      })
+    ) : (
+      <p>Students database is empty</p>
+    );
+  };
+  
 
 export default ListStudents;
